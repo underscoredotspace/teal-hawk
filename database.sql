f22744842ae7a433b0a02d1781973dbc217c90ca
Ampersand Underscore Space <ampersand@underscore.space>
2015-10-19 17:12:15 +0100

    Added current sql schema - needs fixed though

---
+++ database.sql
@@ -0,0 +1,148 @@
+-- MySQL dump 10.15  Distrib 10.0.20-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: twitter
+-- ------------------------------------------------------
+-- Server version	10.0.20-MariaDB-0+deb8u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `json_cache`
+--
+
+DROP TABLE IF EXISTS `json_cache`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `json_cache` (
+  `tweet_id` bigint(20) unsigned NOT NULL,
+  `cache_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cache_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `raw_tweet` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`cache_id`),
+  KEY `tweet_id` (`tweet_id`),
+  KEY `cache_date` (`cache_date`)
+) ENGINE=MyISAM AUTO_INCREMENT=2734 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tweet_mentions`
+--
+
+DROP TABLE IF EXISTS `tweet_mentions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweet_mentions` (
+  `tweet_id` bigint(20) unsigned NOT NULL,
+  `source_user_id` bigint(20) unsigned NOT NULL,
+  `target_user_id` bigint(20) unsigned NOT NULL,
+  KEY `tweet_id` (`tweet_id`),
+  KEY `source` (`source_user_id`),
+  KEY `target` (`target_user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tweet_tags`
+--
+
+DROP TABLE IF EXISTS `tweet_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweet_tags` (
+  `tweet_id` bigint(20) unsigned NOT NULL,
+  `tag` varchar(100) NOT NULL,
+  KEY `tweet_id` (`tweet_id`),
+  KEY `tag` (`tag`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tweet_urls`
+--
+
+DROP TABLE IF EXISTS `tweet_urls`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweet_urls` (
+  `tweet_id` bigint(20) unsigned NOT NULL,
+  `url` varchar(140) NOT NULL,
+  KEY `tweet_id` (`tweet_id`),
+  KEY `url` (`url`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tweets`
+--
+
+DROP TABLE IF EXISTS `tweets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweets` (
+  `tweet_id` bigint(20) unsigned NOT NULL,
+  `tweet_text` varchar(160) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `geo_lat` decimal(10,5) DEFAULT NULL,
+  `geo_long` decimal(10,5) DEFAULT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `screen_name` char(20) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `profile_image_url` varchar(200) DEFAULT NULL,
+  `is_rt` tinyint(1) NOT NULL,
+  PRIMARY KEY (`tweet_id`),
+  KEY `created_at` (`created_at`),
+  KEY `user_id` (`user_id`),
+  KEY `screen_name` (`screen_name`),
+  KEY `name` (`name`),
+  FULLTEXT KEY `tweet_text` (`tweet_text`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` bigint(20) unsigned NOT NULL,
+  `screen_name` varchar(20) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `profile_image_url` varchar(200) DEFAULT NULL,
+  `location` varchar(30) DEFAULT NULL,
+  `url` varchar(200) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `followers_count` int(10) unsigned DEFAULT NULL,
+  `friends_count` int(10) unsigned DEFAULT NULL,
+  `statuses_count` int(10) unsigned DEFAULT NULL,
+  `time_zone` varchar(40) DEFAULT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  KEY `user_name` (`name`),
+  KEY `last_update` (`last_update`),
+  KEY `screen_name` (`screen_name`),
+  FULLTEXT KEY `description` (`description`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-10-19 17:05:21
