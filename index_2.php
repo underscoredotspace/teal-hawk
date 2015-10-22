45bf0f5610dafccf46cd6aa73935f6cb3406c790
Ampersand Underscore Space <ampersand@underscore.space>
2015-10-23 00:10:48 +0100

    Added php wrapper to provide db output as JSON, and AngularJS frontend.

---
+++ index_2.php
@@ -0,0 +1,39 @@
+<html ng-app="tweetApp">
+<head>
+
+<meta charset="utf-8">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="v2.css">
+
+<title>AngularHose (teal-hawk)</title>
+
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+<script>
+  var tweetApp = angular.module('tweetApp', []);
+  tweetApp.controller('TweetCtrl', function ($scope, $http){
+    $http.get('json.php').success(function(data) {
+      $scope.tweets = data;
+    });
+  });
+</script>
+</head>
+
+<body ng-controller="TweetCtrl">
+  <div class="tweet-container">
+    <span class="tweet" ng-repeat="tweet in tweets">
+      <span class="left-column">
+        <img class="profile-image" src="{{tweet.profile_image_url}}" alt="{{tweet.screen_name}}">
+      </span>
+      <span class="right-column">
+        <span class="user">
+          <span class="user-name">{{tweet.name}}</span>
+          <span class="user-handle">{{tweet.screen_name}}</span>
+        </span>
+        <span class="tweet-text" id="tweet-id-{{tweet.tweet_id}}">{{tweet.tweet_text}}</span>
+        <span class="created-at" title="{{tweet.created_at}}">{{tweet.time_ago}}</span>
+      </span>
+    </span>
+  </div>
+</body>
+
+</html>
