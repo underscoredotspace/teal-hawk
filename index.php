11a9e4ed42030841095a2c336a02f6ea3153a063
Ampersand Underscore Space <ampersand@underscore.space>
2015-10-23 09:15:35 +0100

    Fix for issue #7 again. Also removed non-AngularJS version of index

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<html ng-app="tweetApp">
+<!DOCTYPE html>
+<html>
 <head>
 
 <meta charset="utf-8">
@@ -11,18 +12,18 @@
 <script src="ngTweetApp.js"></script>
 </head>
 
-<body ng-controller="TweetCtrl">
+<body data-ng-controller="TweetCtrl" data-ng-app="tweetApp">
   <div class="tweet-container">
-    <span class="tweet" ng-repeat="tweet in tweets">
+    <span class="tweet" data-ng-repeat="tweet in tweets">
       <span class="left-column">
-        <img class="profile-image" src="{{tweet.profile_image_url}}" alt="{{tweet.screen_name}}">
+        <img class="profile-image" src="#" data-ng-src="{{tweet.profile_image_url}}" alt="{{tweet.screen_name}}">
       </span>
       <span class="right-column" >
         <span class="user">
           <span class="user-name">{{tweet.name}}</span>
           <span class="user-handle">{{tweet.screen_name}}</span>
         </span>
-        <span class="tweet-text" id="tweet-id-{{tweet.tweet_id}}" ng-bind-html="tweet.tweet_text | html"></span>
+        <span class="tweet-text" id="tweet-id-{{tweet.tweet_id}}" data-ng-bind-html="tweet.tweet_text | html"></span>
         <span class="created-at" title="{{tweet.created_at}}">{{tweet.time_ago}}</span>
       </span>
     </span>
