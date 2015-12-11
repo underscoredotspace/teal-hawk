e6ae50ff6b6aeea9fb3b2250c15510f2a60277b7
Ampersand Underscore Space <ampersand@underscore.space>
2015-12-11 17:13:33 +0000

    Revert "Added test of recieved newTweet in tweetColumn directive against value in tweet-filter attribute. A number of problems with current approach:"
    This reverts commit 8a27f4dd4ac9e14113c0fa68fa80dca50c356b63.

--- public/tweetApp.directives.js
+++ public/tweetApp.directives.js
@@ -18,8 +18,7 @@ tweetApp.directive('scrollBottom', function () {
             var raw = element[0];
             element.bind('scroll', function () {
                 if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                  
-                  scope.$apply(attrs.scrollBottom);
+                    scope.$apply(attrs.scrollBottom);
                 }
             });
         }
@@ -34,52 +33,36 @@ tweetApp.directive('tweetColumn', function(socket){
     scope: {tweets: '='},
     controller: function ($scope, $attrs) {
       $scope.tweets = [];
-      //console.log($attrs.tweetColumn);
-      socket.emit('initRequest', [$attrs.tweetColumn, 200]);
+      console.log($attrs.tweetColumn);
+      socket.emit('initRequest', [$attrs.tweetColumn, 10]);
       
       socket.on('reconnect', function(){
         if ($scope.tweets!=[]) {
-          console.log('reconnecting column ' + $attrs.tweetColumn + ' ' + $scope.tweets[0].id_str);
+          console.log('reconnecting ' + $scope.tweets[0].id_str);
           socket.emit('updateRequest', [$attrs.tweetColumn, $scope.tweets[0].id_str]);
         } else {
           socket.emit('initRequest', [$attrs.tweetColumn, 10]);
         }
       });
 
-      socket.on('topTweet', function(newTweet) {    
-        if ((newTweet[0]==$attrs.tweetColumn)||(newTweet[0]=='*')){
-          acceptTweet = false;
-          angular.forEach(JSON.parse($attrs.tweetFilter), function(t_value, t_key){
-            if (eval('newTweet[1].' + t_key) == t_value) {
-              acceptTweet = true;     
-            }
-          });
-          if (acceptTweet) {
+      socket.on('topTweet', function(newTweet) {
+        console.log('topTweet recieved');
+          if ((newTweet[0]==$attrs.tweetColumn)||(newTweet[0]=='*')){
             $scope.tweets.unshift(newTweet[1]);
-            $scope.$digest();
-            //console.log('topTweet ' + newTweet[1].id_str + ' to ' + $attrs.tweetColumn);
+            $scope.$digest();    
           }
-        }
       });
 
       socket.on('bottomTweet', function(newTweet) {
+        console.log('bottomTweet recieved');
         if (newTweet[0]==$attrs.tweetColumn){
-          acceptTweet = false;
-          angular.forEach(JSON.parse($attrs.tweetFilter), function(t_value, t_key){
-            if (eval('newTweet[1].' + t_key) == t_value) {
-              acceptTweet = true;     
-            }
-          });
-          if (acceptTweet) {
-            $scope.tweets.push(newTweet[1]);
-            $scope.$digest();
-            //console.log('bottomTweet ' + newTweet[1].id_str + ' to ' + $attrs.tweetColumn);
-          }
-        };  
+          $scope.tweets.push(newTweet[1]);
+          $scope.$digest();
+        }
       });
 
       $scope.showMore = function() {
-        //console.log('Next 10 tweets after ' + $scope.tweets[$scope.tweets.length-1].id + ' please');
+        console.log('Next 10 tweets after ' + $scope.tweets[$scope.tweets.length-1].id + ' please');
         socket.emit('NextTweets', [$attrs.tweetColumn,{last: $scope.tweets[$scope.tweets.length-1].id_str, count: 10}]);
       };
     }
