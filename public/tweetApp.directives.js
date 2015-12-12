b058aadceae2c8126036a7b61abdc60708a05cb7
Ampersand Underscore Space <ampersand@underscore.space>
2015-12-12 16:27:00 +0000

    Created TODO file Moved/edited some debug console.log outputs

--- public/tweetApp.directives.js
+++ public/tweetApp.directives.js
@@ -33,7 +33,6 @@ tweetApp.directive('tweetColumn', function(socket){
     scope: {tweets: '='},
     controller: function ($scope, $attrs) {
       $scope.tweets = [];
-      console.log($attrs.tweetColumn);
       socket.emit('initRequest', [$attrs.tweetColumn, 10]);
       
       socket.on('reconnect', function(){
@@ -46,23 +45,23 @@ tweetApp.directive('tweetColumn', function(socket){
       });
 
       socket.on('topTweet', function(newTweet) {
-        console.log('topTweet recieved');
-          if ((newTweet[0]==$attrs.tweetColumn)||(newTweet[0]=='*')){
-            $scope.tweets.unshift(newTweet[1]);
-            $scope.$digest();    
-          }
+        if ((newTweet[0]==$attrs.tweetColumn)||(newTweet[0]=='*')){
+          $scope.tweets.unshift(newTweet[1]);
+          $scope.$digest();    
+          console.log('topTweet recieved for column ' + $attrs.tweetColumn);
+        }
       });
 
       socket.on('bottomTweet', function(newTweet) {
-        console.log('bottomTweet recieved');
         if (newTweet[0]==$attrs.tweetColumn){
           $scope.tweets.push(newTweet[1]);
           $scope.$digest();
+          console.log('bottomTweet recieved ' + $attrs.tweetColumn);
         }
       });
 
       $scope.showMore = function() {
-        console.log('Next 10 tweets after ' + $scope.tweets[$scope.tweets.length-1].id + ' please');
+        console.log('Next 10 tweets after ' + $scope.tweets[$scope.tweets.length-1].id + ' requested');
         socket.emit('NextTweets', [$attrs.tweetColumn,{last: $scope.tweets[$scope.tweets.length-1].id_str, count: 10}]);
       };
     }
