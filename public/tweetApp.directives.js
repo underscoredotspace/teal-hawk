b1ac6ba098141a434ace994328d2732582b219d3
Ampersand Underscore Space <ampersand@underscore.space>
2015-12-20 18:34:08 +0000

    Fix for issues #41 and #43
    Ugliness fixed - iOS is adding padding to INPUT boxes.
    Clear button given background and text colours due to inconsistency
    between browsers, on some face is the same colour as my background.
    Issue #43 noticed while testing #41’s fix. $timeout delay seems to be
    the issue so removed. Doesn’t seem to have negative affect on
    performance.

--- public/tweetApp.directives.js
+++ public/tweetApp.directives.js
@@ -3,26 +3,24 @@ tweetApp.directive('focusInput', function($timeout) {
     restrict: 'A',
     link: function(scope, element, attrs) {
       element.bind('click', function() {
-        $timeout(function() {
-          element.parent().find('input')[0].focus();
-        });
+        element.parent().find('input')[0].focus();
       });
     }
   };
 });
 
 tweetApp.directive('scrollBottom', function () {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            var raw = element[0];
-            element.bind('scroll', function () {
-                if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                    scope.$apply(attrs.scrollBottom);
-                }
-            });
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var raw = element[0];
+      element.bind('scroll', function () {
+        if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+          scope.$apply(attrs.scrollBottom);
         }
-    };
+      });
+    }
+  };
 });
 
 tweetApp.directive('tweetColumn', function(socket, $timeout){
