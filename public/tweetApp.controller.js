5f7097835f63cf45ea24e608a8f0623416b7d9a7
Ampersand Underscore Space <ampersand@underscore.space>
2015-12-25 18:41:43 +0000

    Mistakenly removed columns declaration in client
    Controller needs to create these values for now. Will be requesting
    values from mongodb.

--- public/tweetApp.controller.js
+++ public/tweetApp.controller.js
@@ -12,4 +12,16 @@ tweetApp.controller('tweetController', function ($scope, $filter, socket){
   socket.on('connect_error', function(err){
     console.log('connection error: ' + err);
   });
+  
+  $scope.columns = [{
+    "id": "fdac",
+    "position": 2,
+    "type": "tweetColumn",
+    "parameters": '{"$or":[{"user.id_str":"284537825"},{"entities.user_mentions.id_str":"284537825"}]}'
+  },{
+    "id": "e0b1",
+    "position": 1,
+    "type": "tweetColumn",
+    "parameters": '{"$or":[{"user.id_str":"284540385"},{"entities.user_mentions.id_str":"284540385"}]}'
+  }];
 });
