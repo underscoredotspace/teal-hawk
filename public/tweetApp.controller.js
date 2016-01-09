7a800bb3a671e45b634fb751d7822fc0f5580bb0
Ampersand Underscore Space <ampersand@underscore.space>
2016-01-09 00:53:42 +0000

    Created single column view with angular-touch
    Designed for mobile, but to be enabled only for small screens. Swipe to
    move left or right between columns.

--- public/tweetApp.controller.js
+++ public/tweetApp.controller.js
@@ -2,7 +2,7 @@
 //   Moment used for nice time presentation
 //   Animate for visual introduction of new data to view
 
-var tweetApp = angular.module('tweetApp', ['angularMoment', 'ngAnimate']);
+var tweetApp = angular.module('tweetApp', ['angularMoment', 'ngAnimate', 'ngTouch']);
 
 tweetApp.controller('tweetController', function ($scope, $filter, socket){
   socket.on('connect', function(){
@@ -15,15 +15,51 @@ tweetApp.controller('tweetController', function ($scope, $filter, socket){
   
   $scope.columns = [{
     "id": "fdac",
-    "position": 2,
+    "position": 1,
     "type": "tweetColumn",
     "parameters": '{\"$or\":[{\"user.id_str\":\"284537825\"},{\"entities.user_mentions.id_str\":\"284537825\"}]}'
   },{
     "id": "e0b1",
-    "position": 1,
+    "position": 2,
     "type": "tweetColumn",
     "parameters": '{\"$or\":[{\"user.id_str\":\"284540385\"},{\"entities.user_mentions.id_str\":\"284540385\"}]}'
   }];
+  
+  $scope[$scope.columns[0].id] = true;
+  
+  $scope.next = function(colID) {
+    currentColID = colID;
+    colID++;
+    if (colID>$scope.columns.length-1) {
+      colID = 0;
+    }
+    // Add slide LEFT animation class
+    // @TODO
+    
+    // Trigger actual ng-show stuff
+    $scope[$scope.columns[currentColID].id]=false;
+    $scope[$scope.columns[colID].id]=true;
+    
+    // Remove slide LEFT animation class
+    // @TODO
+  }
+  
+  $scope.prev = function(colID) {
+    currentColID = colID;
+    colID--;
+    if (colID<0) {
+      colID = $scope.columns.length-1;
+    }
+    // Add slide RIGHT animation class
+    // @TODO
+    
+    // Trigger actual ng-show stuff
+    $scope[$scope.columns[currentColID].id]=false;
+    $scope[$scope.columns[colID].id]=true;
+    
+    // Remove slide RIGHT animation class
+    // @TODO
+  }
   
   $scope.criteria = [];
   for (var count = 0; count <= $scope.columns.length-1; count++) {
